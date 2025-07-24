85e90992e6a6b64545e16daf7e951a3198bb5ceb
Reazul-Islam-Fahim <jkabbo@yahoo.com>
2025-07-24 13:47:21 +0600

    update

--- frontend/src/components/UploadProject.js
+++ frontend/src/components/UploadProject.js
@@ -1,68 +1,3 @@
-
-// import React, { useState } from 'react';
-// import axios from '../api';
-
-// const UploadProject = ({ onUploadSuccess }) => {
-//   const [title, setTitle] = useState('');
-//   const [desc, setDesc] = useState('');
-//   const [file, setFile] = useState(null);
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     if (!file) return alert('Choose a file');
-
-//     const formData = new FormData();
-//     formData.append('title', title);
-//     formData.append('description', desc);
-//     formData.append('image_file', file);
-
-//     try {
-//       const res = await axios.post('/projects/upload/', formData, {
-//         headers: { 'Content-Type': 'multipart/form-data' },
-//       });
-//       alert('Uploaded successfully');
-//       onUploadSuccess(res.data);
-//     } catch (err) {
-//       console.error(err);
-//       alert('Failed to upload');
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
-//       <h3>Create New Project</h3>
-//       <input
-//         type="text"
-//         placeholder="Project Title"
-//         onChange={(e) => setTitle(e.target.value)}
-//         required
-//         style={{ width: '100%', marginBottom: 10 }}
-//       />
-//       <textarea
-//         placeholder="Description"
-//         onChange={(e) => setDesc(e.target.value)}
-//         style={{ width: '100%', height: 80, marginBottom: 10 }}
-//       />
-//       <input
-//         type="file"
-//         accept="image/*"
-//         onChange={(e) => setFile(e.target.files[0])}
-//         required
-//         style={{ marginBottom: 10 }}
-//       />
-//       <br />
-//       <button type="submit">Upload</button>
-//     </form>
-//   );
-// };
-
-// export default UploadProject;
-
-
-
-// ___________________________________________________________________________
-
-
 import React, { useState } from 'react';
 import axios from '../api';
 
@@ -93,7 +28,8 @@ const UploadProject = ({ onUploadSuccess }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
+    <div style={{ padding: '20px', borderRadius: '8px', display: 'flex' }}>
+    <form onSubmit={handleSubmit} style={{ Width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column' , border: '1px solid #ccc', padding: '20px', borderRadius: '8px', backgroundColor: '#f9f9f9', boxShadow: '0 2px 4px rgba(0,0,0,0.1)'}}>
       <h3>Create New Project</h3>
       <input
         type="text"
@@ -119,6 +55,7 @@ const UploadProject = ({ onUploadSuccess }) => {
       <br />
       <button type="submit">Upload</button>
     </form>
+    </div>
   );
 };
 
